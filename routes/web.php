4b522c53906561f0c1b4763e945135b4f050c87b
byy7 <mfarabyy@gmail.com>
2026-01-21 19:33:27 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('livewire.auth.login');
+})->name('home');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+require __DIR__.'/settings.php';
