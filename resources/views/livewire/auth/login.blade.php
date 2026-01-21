d1f682e977d66f0770fc997c74f94440dcafed21
byy7 <mfarabyy@gmail.com>
2026-01-21 21:57:29 +0700

    feat: add library & change lang into Indonesia

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,9 +1,12 @@
 <x-layouts::auth>
+    <div class="absolute inset-0 -z-10 bg-cover bg-center bg-no-repeat"
+         style="background-image: url('{{ asset('assets/img/gedung1.webp') }}');opacity: 35%"></div>
+
     <div class="flex flex-col gap-6">
-        <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
+        <x-auth-header :title="__('Login')" :description="__('Silahkan masukkan akun anda')"/>
 
         <!-- Session Status -->
-        <x-auth-session-status class="text-center" :status="session('status')" />
+        <x-auth-session-status class="text-center" :status="session('status')"/>
 
         <form method="POST" action="{{ route('login.store') }}" class="flex flex-col gap-6">
             @csrf
@@ -11,13 +14,13 @@
             <!-- Email Address -->
             <flux:input
                 name="email"
-                :label="__('Email address')"
+                :label="__('Email')"
                 :value="old('email')"
                 type="email"
                 required
                 autofocus
                 autocomplete="email"
-                placeholder="email@example.com"
+                placeholder="Masukkan Email"
             />
 
             <!-- Password -->
@@ -28,31 +31,31 @@
                     type="password"
                     required
                     autocomplete="current-password"
-                    :placeholder="__('Password')"
+                    :placeholder="__('Masukkan Password')"
                     viewable
                 />
 
-                @if (Route::has('password.request'))
-                    <flux:link class="absolute top-0 text-sm end-0" :href="route('password.request')" wire:navigate>
-                        {{ __('Forgot your password?') }}
-                    </flux:link>
-                @endif
+                {{--                @if (Route::has('password.request'))--}}
+                {{--                    <flux:link class="absolute top-0 text-sm end-0" :href="route('password.request')" wire:navigate>--}}
+                {{--                        {{ __('Forgot your password?') }}--}}
+                {{--                    </flux:link>--}}
+                {{--                @endif--}}
             </div>
 
             <!-- Remember Me -->
-            <flux:checkbox name="remember" :label="__('Remember me')" :checked="old('remember')" />
+            <flux:checkbox name="remember" :label="__('Ingat Saya')" :checked="old('remember')"/>
 
             <div class="flex items-center justify-end">
-                <flux:button variant="primary" type="submit" class="w-full" data-test="login-button">
-                    {{ __('Log in') }}
+                <flux:button variant="primary" type="submit" class="w-full">
+                    {{ __('Login') }}
                 </flux:button>
             </div>
         </form>
 
         @if (Route::has('register'))
             <div class="space-x-1 text-sm text-center rtl:space-x-reverse text-zinc-600 dark:text-zinc-400">
-                <span>{{ __('Don\'t have an account?') }}</span>
-                <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
+                <span>{{ __('Belum mempunyai akun?') }}</span>
+                <flux:link :href="route('register')" wire:navigate>{{ __('Daftar') }}</flux:link>
             </div>
         @endif
     </div>
--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -1,6 +1,9 @@
 <x-layouts::auth>
+    <div class="absolute inset-0 -z-10 bg-cover bg-center bg-no-repeat"
+         style="background-image: url('{{ asset('assets/img/gedung1.webp') }}');opacity: 35%"></div>
+
     <div class="flex flex-col gap-6">
-        <x-auth-header :title="__('Create an account')" :description="__('Enter your details below to create your account')" />
+        <x-auth-header :title="__('Buat Akun')" :description="__('Silahkan isi form dibawah untuk membuat akun')" />
 
         <!-- Session Status -->
         <x-auth-session-status class="text-center" :status="session('status')" />
@@ -11,24 +14,24 @@
             <!-- Name -->
             <flux:input
                 name="name"
-                :label="__('Name')"
+                :label="__('Nama')"
                 :value="old('name')"
                 type="text"
                 required
                 autofocus
                 autocomplete="name"
-                :placeholder="__('Full name')"
+                :placeholder="__('Masukkan Nama')"
             />
 
             <!-- Email Address -->
             <flux:input
                 name="email"
-                :label="__('Email address')"
+                :label="__('Email')"
                 :value="old('email')"
                 type="email"
                 required
                 autocomplete="email"
-                placeholder="email@example.com"
+                placeholder="Masukkan Email"
             />
 
             <!-- Password -->
@@ -45,24 +48,24 @@
             <!-- Confirm Password -->
             <flux:input
                 name="password_confirmation"
-                :label="__('Confirm password')"
+                :label="__('Konfirmasi Password')"
                 type="password"
                 required
                 autocomplete="new-password"
-                :placeholder="__('Confirm password')"
+                :placeholder="__('Konfirmasi Password')"
                 viewable
             />
 
             <div class="flex items-center justify-end">
                 <flux:button type="submit" variant="primary" class="w-full">
-                    {{ __('Create account') }}
+                    {{ __('Buat Akun') }}
                 </flux:button>
             </div>
         </form>
 
         <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
-            <span>{{ __('Already have an account?') }}</span>
-            <flux:link :href="route('login')" wire:navigate>{{ __('Log in') }}</flux:link>
+            <span>{{ __('Sudah mempunyai akun?') }}</span>
+            <flux:link :href="route('login')" wire:navigate>{{ __('Login') }}</flux:link>
         </div>
     </div>
 </x-layouts::auth>
